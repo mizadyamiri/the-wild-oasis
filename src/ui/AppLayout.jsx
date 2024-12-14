37bacfc3352c6dc34a651723aecca119346081d0
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2024-12-14 15:01:16 +0330

    style: style applayout with styled-components and react-icons

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,12 +1,30 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import styled from 'styled-components';
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
-    <div>
-      <p>App Layout</p>
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
 
-      <Outlet />
-    </div>
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
