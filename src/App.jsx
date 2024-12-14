292b110db3fb363dd29d7d42c0c3ea630d3c0379
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2024-12-14 12:10:45 +0330

    chore: add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+
+const StyledApp = styled.main`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
+
+            <div>
+              <Heading as='h2'>check in and out</Heading>
+              <Button variation='primary' size='medium'>
+                Check in
+              </Button>
+              <Button variation='secondary' size='small'>
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row type='vertical'>
+            <Heading as='h3'>form</Heading>
+
+            <form>
+              <Input type='number' placeholder='Number of guest'></Input>
+              <Input type='number' placeholder='Number of guest'></Input>
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
