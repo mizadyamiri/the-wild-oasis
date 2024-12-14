f890030cadcf41604cb83e87e85ec64c55d55fe8
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2024-12-14 12:44:12 +0330

    chore: add react-router-dom@6

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+function AppLayout() {
+  return (
+    <div>
+      <p>App Layout</p>
+
+      <Outlet />
+    </div>
+  );
+}
+
+export default AppLayout;
