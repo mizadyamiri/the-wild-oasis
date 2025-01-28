f2f9498b777afffb0617d229c04c76a893cb9adf
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2025-01-29 02:12:18 +0330

    feat: add api side filter/sort to bookings table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { getbookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getbookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getbookings({ filter, sortBy }),
   });
 
   return { bookings, isLoading, error };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,21 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getbookings() {
-  const { data, error } = await supabase
+export async function getbookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
+
+  // Filter
+  if (filter) query = query.eq(filter.field, filter.value);
+
+  // Sort
+  if (sortBy)
+    query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
