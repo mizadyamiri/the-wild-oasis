30c047d1f2c7466de41f74118da46579d2926f41
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2025-01-30 23:54:43 +0330

    feat: authorization with protected route

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
-// QueryClient sets up the cache behind the scene
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 0 } },
 });
@@ -29,7 +29,13 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to='/dashboard' />} />
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='bookings' element={<Bookings />} />
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import { useEffect } from "react";
+import { useUser } from "../features/authentication/useUser";
+import { useNavigate } from "react-router-dom";
+
+import styled from "styled-components";
+import Spinner from "./Spinner";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { isLoading, isAuthenticated } = useUser();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/login");
+  }, [isAuthenticated, isLoading, navigate]);
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
