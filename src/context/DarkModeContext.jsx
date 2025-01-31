4d2f3af0052fe59e14b4293bc847fad8f08ffc7e
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2025-01-31 17:46:30 +0330

    feat: add dark/light mode

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(p => !p);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined) throw new Error("useDarkMode used outside it's provider.");
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
