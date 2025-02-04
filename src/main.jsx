12b807ae9a4b7944ac7363b30828e8be054de3d5
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2025-02-04 12:36:36 +0330

    fix: error boundry reset link url

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary
       fallbackRender={ErrorFallback}
-      onReset={() => window.location.replace("/")}
+      onReset={() => window.location.replace(import.meta.env.BASE_URL)}
     >
       <App />
     </ErrorBoundary>
